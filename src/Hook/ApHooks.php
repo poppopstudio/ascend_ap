894728961000767907886b18db172b7fce0a9df9
NikLP <NikLP@users.noreply.github.com>
2025-12-05 17:27:43 +0000

    Add code to remove comments from display (PDF)

--- src/Hook/ApHooks.php
+++ src/Hook/ApHooks.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\ascend_ap\Hook;
 
+use Drupal\Core\Entity\Entity\EntityViewDisplay;
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Hook\Attribute\Hook;
 
@@ -18,4 +20,18 @@ class ApHooks {
     return;
   }
 
+
+  /**
+   * Implements hook_entity_view_alter().
+   */
+  #[Hook('entity_view_alter')]
+  function entityViewAlter(array &$build, EntityInterface $entity, EntityViewDisplay $display) {
+    // Check if we're in an entity_print context
+    if (\Drupal::routeMatch()->getRouteName() == 'entity_print.view') {
+      // Remove comment field from the build array
+      if (isset($build['ascend_ap_comments'])) {
+        unset($build['ascend_ap_comments']);
+      }
+    }
+  }
 }
